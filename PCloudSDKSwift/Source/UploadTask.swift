e6f088a7a7e0a43d845dc7385b878c9a2396a6cd
Todor Pitekov <esembidi@gmail.com>
2017-11-27 12:57:07 +0200

    master: use CallError in UploadTask

--- PCloudSDKSwift/Source/UploadTask.swift
+++ PCloudSDKSwift/Source/UploadTask.swift
@@ -36,25 +36,25 @@ public final class UploadTask<Method: PCloudApiMethod>: Cancellable {
 	/// - parameter block: A block called on the main thread with the result of the task.
 	/// - returns: This task.
 	@discardableResult
-	public func setCompletionBlock(_ block: @escaping (Result<Method.Value, Error>) -> Void) -> UploadTask {
+	public func setCompletionBlock(_ block: @escaping (Result<Method.Value, CallError<Method.Error>>) -> Void) -> UploadTask {
 		let parse = self.parse
 		
 		// Parse the response on a background queue.
 		operation.setCompletionBlock(queue: .global()) { response in
 			// Compute the response.
-			let result: Result<Method.Value, Error> = {
+			let result: Result<Method.Value, CallError<Method.Error>> = {
 				switch response {
 				case .success(let response):
 					do {
 						return .success(try parse(response))
 					} catch {
-						return .failure(error)
+						return .failure(.clientError(error))
 					}
 					
 				case .failure(let error):
 					switch error {
-					case .clientError(let ce): return .failure(ce)
-					case .protocolError(let pe): return .failure(pe)
+					case .clientError(let ce): return .failure(.clientError(ce))
+					case .protocolError(let pe): return .failure(.protocolError(pe))
 					}
 				}
 			}()
