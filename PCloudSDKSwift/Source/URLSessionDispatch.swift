1dcd88a0fd451eae6c2bf9137f6bd2c117e642c4
Todor Pitekov <esembidi@gmail.com>
2017-11-28 14:21:07 +0200

    master: move url session task creation logic to a separate file

--- PCloudSDKSwift/Source/URLSessionDispatch.swift
+++ PCloudSDKSwift/Source/URLSessionDispatch.swift
@@ -8,12 +8,6 @@
 
 import Foundation
 
-/// A URL scheme.
-public enum Scheme: String {
-	case http = "http"
-	case https = "https"
-}
-
 /// Contains logic for building `URLSessionTask`-backed network operations from network requests.
 public struct URLSessionDispatch {
 	/// Creates and returns a block that builds a `CallOperation` from a `Call.Request` using a `URLSession`.
@@ -24,19 +18,7 @@ public struct URLSessionDispatch {
 	/// - returns: A block that builds a `URLSessionDataTask`-backed `CallOperation` from a `Call.Request`.
 	public static func callOperationBuilder(scheme: Scheme, session: URLSession, delegate: URLSessionEventHub) -> (Call.Request) -> CallOperation {
 		return { request in
-			// Build the URL.
-			let url = self.url(scheme: scheme.rawValue, host: request.hostName, commandName: request.command.name)
-			// Build a form data encoded query.
-			let query = self.query(from: request.command.parameters, addingPercentEncoding: true)
-			
-			// Build a POST request.
-			var urlRequest = URLRequest(url: url)
-			urlRequest.httpMethod = HTTPMethod.post
-			urlRequest.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-			urlRequest.httpBody = query.data(using: .utf8, allowLossyConversion: false)
-			
-			// Build the operation and assign it as observer to the data task.
-			let task = session.dataTask(with: urlRequest)
+			let task = URLSessionTaskBuilder.createDataTask(request: request, session: session, scheme: scheme)
 			let operation = URLSessionBasedCallOperation(task: task)
 			delegate.setObserver(operation, for: task)
 			
@@ -52,24 +34,7 @@ public struct URLSessionDispatch {
 	/// - returns: A block that builds a `URLSessionUploadTask`-backed `UploadOperation` from an `Upload.Request`.
 	public static func uploadOperationBuilder(scheme: Scheme, session: URLSession, delegate: URLSessionEventHub) -> (Upload.Request) -> UploadOperation {
 		return { request in
-			// Build a GET query.
-			let query = self.query(from: request.command.parameters, addingPercentEncoding: false)
-			// Build the URL.
-			let url = self.url(scheme: scheme.rawValue, host: request.hostName, commandName: request.command.name, query: query)
-			
-			// Build a POST request.
-			var urlRequest = URLRequest(url: url)
-			urlRequest.httpMethod = HTTPMethod.post
-			urlRequest.setValue("applicaton/octet-stream", forHTTPHeaderField: "Content-Type")
-			
-			// Build the operation and assign it as observer to the upload task.
-			let task: URLSessionUploadTask
-			
-			switch request.body {
-			case .file(let url): task = session.uploadTask(with: urlRequest, fromFile: url)
-			case .data(let data): task = session.uploadTask(with: urlRequest, from: data)
-			}
-			
+			let task = URLSessionTaskBuilder.createUploadTask(request: request, session: session, scheme: scheme)
 			let operation = URLSessionBasedUploadOperation(task: task)
 			delegate.setObserver(operation, for: task)
 			
@@ -77,83 +42,18 @@ public struct URLSessionDispatch {
 		}
 	}
 	
+	/// Creates and returns a block that builds a `DownloadOperation` from a `Download.Request` using a `URLSession`.
+	///
+	/// - parameter session: A session used to create download tasks.
+	/// - parameter delegate: The session delegate.
+	/// - returns: A block that builds a `URLSessionDownloadTask`-backed `DownloadOperation` from a `Download.Request`.
 	public static func downloadOperationBuilder(session: URLSession, delegate: URLSessionEventHub) -> (Download.Request) -> DownloadOperation {
 		return { request in
-			// Build the operation and assign it as observer to the upload task.
-			let task = session.downloadTask(with: request.resourceAddress)
+			let task = URLSessionTaskBuilder.createDownloadTask(request: request, session: session)
 			let operation = URLSessionBasedDownloadOperation(task: task, destination: request.destination)
 			delegate.setObserver(operation, for: task)
 			
 			return operation
 		}
-	}
-	
-	
-	// Functions for encoding command parameter values to percent encoded strings.
-	
-	static func encode(_ value: String) -> String {
-		return value.addingPercentEncoding(withAllowedCharacters: .urlQueryParameterAllowedCharacterSetRFC3986())!
-	}
-	
-	static func encode(_ value: Bool) -> String {
-		return value ? "1" : "0"
-	}
-	
-	static func encode(_ value: UInt64) -> String {
-		return "\(value)"
-	}
-	
-	
-	// Functions for building a query string from command parameters.
-	
-	static func rawQueryComponent(from parameter: Call.Command.Parameter) -> String {
-		switch parameter {
-		case let .boolean(name, value): return "\(name)=\(encode(value))"
-		case let .number(name, value): return "\(name)=\(encode(value))"
-		case let .string(name, value): return "\(name)=\(value)"
-		}
-	}
-	
-	static func percentEncodedQueryComponent(from parameter: Call.Command.Parameter) -> String {
-		switch parameter {
-		case let .boolean(name, value): return "\(encode(name))=\(encode(value))"
-		case let .number(name, value): return "\(encode(name))=\(encode(value))"
-		case let .string(name, value): return "\(encode(name))=\(encode(value))"
-		}
-	}
-	
-	static func query(from parameters: [Call.Command.Parameter], addingPercentEncoding encode: Bool) -> String {
-		let components: [String]
-		
-		if encode {
-			components = parameters.map(percentEncodedQueryComponent)
-		} else {
-			components = parameters.map(rawQueryComponent)
-		}
-		
-		return components.joined(separator: "&")
-	}
-	
-	static func url(scheme: String, host: String, commandName: String, query: String? = nil) -> URL {
-		var components = URLComponents()
-		components.scheme = scheme
-		components.host = host
-		components.path = "/\(commandName)"
-		components.query = query
-		
-		return components.url!
-	}
-}
-
-struct HTTPMethod {
-	static let get = "GET"
-	static let post = "POST"
-	static let put = "PUT"
-}
-
-extension CharacterSet {
-	// A character with characters allowed in a URL query as per RFC 3986.
-	static func urlQueryParameterAllowedCharacterSetRFC3986() -> CharacterSet {
-		return self.init(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~/?")
 	}
 }
---
+++ PCloudSDKSwift/Source/URLSessionTaskBuilder.swift
@@ -0,0 +1,121 @@
+//
+//  URLSessionTaskBuilder.swift
+//  SDK iOS
+//
+//  Created by Todor Pitekov on 11/28/17.
+//  Copyright © 2017 pCloud LTD. All rights reserved.
+//
+
+import Foundation
+
+/// A URL scheme.
+public enum Scheme: String {
+	case http = "http"
+	case https = "https"
+}
+
+public struct URLSessionTaskBuilder {
+	public static func createDataTask(request: Call.Request, session: URLSession, scheme: Scheme) -> URLSessionDataTask {
+		// Build the URL.
+		let url = self.url(scheme: scheme.rawValue, host: request.hostName, commandName: request.command.name)
+		// Build a form data encoded query.
+		let query = self.query(from: request.command.parameters, addingPercentEncoding: true)
+		
+		// Build a POST request.
+		var urlRequest = URLRequest(url: url)
+		urlRequest.httpMethod = HTTPMethod.post
+		urlRequest.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+		urlRequest.httpBody = query.data(using: .utf8, allowLossyConversion: false)
+		
+		return session.dataTask(with: urlRequest)
+	}
+	
+	public static func createUploadTask(request: Upload.Request, session: URLSession, scheme: Scheme) -> URLSessionUploadTask {
+		// Build a GET query.
+		let query = self.query(from: request.command.parameters, addingPercentEncoding: false)
+		// Build the URL.
+		let url = self.url(scheme: scheme.rawValue, host: request.hostName, commandName: request.command.name, query: query)
+		
+		// Build a POST request.
+		var urlRequest = URLRequest(url: url)
+		urlRequest.httpMethod = HTTPMethod.post
+		urlRequest.setValue("applicaton/octet-stream", forHTTPHeaderField: "Content-Type")
+		
+		switch request.body {
+		case .file(let url): return session.uploadTask(with: urlRequest, fromFile: url)
+		case .data(let data): return session.uploadTask(with: urlRequest, from: data)
+		}
+	}
+	
+	public static func createDownloadTask(request: Download.Request, session: URLSession) -> URLSessionDownloadTask {
+		return session.downloadTask(with: request.resourceAddress)
+	}
+	
+	// Functions for encoding command parameter values to percent encoded strings.
+	
+	public static func encode(_ value: String) -> String {
+		return value.addingPercentEncoding(withAllowedCharacters: .urlQueryParameterAllowedCharacterSetRFC3986())!
+	}
+	
+	public static func encode(_ value: Bool) -> String {
+		return value ? "1" : "0"
+	}
+	
+	public static func encode(_ value: UInt64) -> String {
+		return "\(value)"
+	}
+	
+	
+	// Functions for building a query string from command parameters.
+	
+	public static func rawQueryComponent(from parameter: Call.Command.Parameter) -> String {
+		switch parameter {
+		case let .boolean(name, value): return "\(name)=\(encode(value))"
+		case let .number(name, value): return "\(name)=\(encode(value))"
+		case let .string(name, value): return "\(name)=\(value)"
+		}
+	}
+	
+	public static func percentEncodedQueryComponent(from parameter: Call.Command.Parameter) -> String {
+		switch parameter {
+		case let .boolean(name, value): return "\(encode(name))=\(encode(value))"
+		case let .number(name, value): return "\(encode(name))=\(encode(value))"
+		case let .string(name, value): return "\(encode(name))=\(encode(value))"
+		}
+	}
+	
+	public static func query(from parameters: [Call.Command.Parameter], addingPercentEncoding encode: Bool) -> String {
+		let components: [String]
+		
+		if encode {
+			components = parameters.map(percentEncodedQueryComponent)
+		} else {
+			components = parameters.map(rawQueryComponent)
+		}
+		
+		return components.joined(separator: "&")
+	}
+	
+	public static func url(scheme: String, host: String, commandName: String, query: String? = nil) -> URL {
+		var components = URLComponents()
+		components.scheme = scheme
+		components.host = host
+		components.path = "/\(commandName)"
+		components.query = query
+		
+		return components.url!
+	}
+}
+
+struct HTTPMethod {
+	static let get = "GET"
+	static let post = "POST"
+	static let put = "PUT"
+}
+
+public extension CharacterSet {
+	// A character with characters allowed in a URL query as per RFC 3986.
+	static func urlQueryParameterAllowedCharacterSetRFC3986() -> CharacterSet {
+		return self.init(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~/?")
+	}
+}
