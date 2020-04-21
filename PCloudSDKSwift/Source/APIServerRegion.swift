ebce17d65a1cbc57b287d0c62a99266e150597ff
Todor Pitekov <todor.pitekov@pcloud.com>
2020-04-21 13:30:08 +0300

    add api server region enum

---
+++ PCloudSDKSwift/Source/APIServerRegion.swift
@@ -0,0 +1,14 @@
+//
+//  APIServerRegion.swift
+//  PCloudSDKSwift
+//
+//  Created by Todor Pitekov on 21.04.20.
+//  Copyright © 2020 pCloud LTD. All rights reserved.
+//
+
+import Foundation
+
+public enum APIServerRegion: Int {
+	case unitedStates = 1
+	case europe = 2
+}
