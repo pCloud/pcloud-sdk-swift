0c9a66e672f8a7dd5cd9d68fe824b03a78f510c1
Todor Pitekov <todor.pitekov@pcloud.com>
2020-04-24 15:32:22 +0300

    make Keychain internal

--- PCloudSDKSwift/Source/Keychain.swift
+++ PCloudSDKSwift/Source/Keychain.swift
@@ -9,12 +9,12 @@
 import Foundation
 
 /// A simple wrapper around the keychain C API.
-public struct Keychain {
+struct Keychain {
 	/// Fetches and returns data for a key.
 	///
 	/// - parameter key: The key to check against.
 	/// - returns: The data found against `key`, or `nil` if no entry was found for `key`.
-	public static func getData(forKey key: String) -> Data? {
+	static func getData(forKey key: String) -> Data? {
 		let query = self.query(attributes: [
 			kSecAttrAccount as String: key,
 			kSecReturnData as String: kCFBooleanTrue!,
@@ -39,7 +39,7 @@ public struct Keychain {
 	/// - parameter value: The data to store.
 	/// - parameter key: The key to store `value` against.
 	/// - returns: Whether the operation was successful.
-	@discardableResult public static func set(_ value: Data, forKey key: String) -> Bool {
+	@discardableResult static func set(_ value: Data, forKey key: String) -> Bool {
 		let query = self.query(attributes: [
 			kSecAttrAccount as String: key,
 			kSecValueData as String: value
@@ -54,7 +54,7 @@ public struct Keychain {
 	///
 	/// - parameter key: The key identifying the entry to remove.
 	/// - returns: Whether the operation was successful.
-	@discardableResult public static func deleteData(forKey key: String) -> Bool {
+	@discardableResult static func deleteData(forKey key: String) -> Bool {
 		let query = self.query(attributes: [kSecAttrAccount as String: key])
 		return SecItemDelete(query) == noErr
 	}
@@ -62,7 +62,7 @@ public struct Keychain {
 	/// Fetches and returns all keys stored in the namespace defined by an instance of this keychain.
 	///
 	/// - returns: An array of all keys stored in the namespace defined by an instance of this keychain or an empty array on error.
-	public static func getAllKeys() -> [String] {
+	static func getAllKeys() -> [String] {
 		let query = self.query(attributes: [
 			kSecReturnAttributes as String: kCFBooleanTrue!,
 			kSecMatchLimit as String: kSecMatchLimitAll
@@ -82,7 +82,7 @@ public struct Keychain {
 		return dictionary.map { $0[kSecAttrAccount as String] as! String }
 	}
 	
-	private static func query(attributes: [String: Any]) -> CFDictionary {
+	static func query(attributes: [String: Any]) -> CFDictionary {
 		var copy = attributes
 		
 		copy[kSecAttrService as String] = "\(Bundle.main.bundleIdentifier ?? "").pcloud"
@@ -100,7 +100,7 @@ extension Keychain {
 	/// - parameter value: The string to store.
 	/// - parameter key: The key to store `value` against.
 	/// - returns: Whether the operation was successful.
-	@discardableResult public static func set(_ value: String, forKey key: String) -> Bool {
+	@discardableResult static func set(_ value: String, forKey key: String) -> Bool {
 		if let data = value.data(using: .utf8) {
 			return set(data, forKey: key)
 		}
@@ -112,7 +112,7 @@ extension Keychain {
 	///
 	/// - parameter key: The key to check against.
 	/// - returns: The data against `key` as a UTF8 string.
-	public static func getString(forKey key: String) -> String? {
+	static func getString(forKey key: String) -> String? {
 		if let data = getData(forKey: key) {
 			return String(data: data, encoding: .utf8)
 		}
