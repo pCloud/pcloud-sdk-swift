fc552a172a8ab28cca014dcc50f8841e38ba51e5
Todor Pitekov <todor.pitekov@pcloud.com>
2021-04-25 12:28:49 +0300

    go through NSNumber when parsing numbers

--- PCloudSDKSwift/Source/Common/Parser.swift
+++ PCloudSDKSwift/Source/Common/Parser.swift
@@ -30,50 +30,25 @@ extension Dictionary where Key == String, Value == Any {
 	///
 	/// - parameter key: A key for the value.
 	/// - returns: The value cast to T, or `nil` if nothing is mapped against `key`.
-	public func value<T>(_ key: String) -> T? { return self[key] as! T? }
+	public func value<T>(_ key: String) -> T? { self[key] as! T? }
 	
-	public func intOrNil(_ key: String) -> Int? { return value(key) }
-	public func boolOrNil(_ key: String) -> Bool? { return value(key) }
-	public func stringOrNil(_ key: String) -> String? { return value(key) }
+	public func intOrNil(_ key: String) -> Int? { numberOrNil(key)?.intValue }
+	public func boolOrNil(_ key: String) -> Bool? { numberOrNil(key)?.boolValue }
+	public func stringOrNil(_ key: String) -> String? { value(key) }
+	public func floatOrNil(_ key: String) -> Float? { numberOrNil(key)?.floatValue }
+	public func uintOrNil(_ key: String) -> UInt? { numberOrNil(key)?.uintValue }
+	public func uint32OrNil(_ key: String) -> UInt32? { numberOrNil(key)?.uint32Value }
+	public func uint64OrNil(_ key: String) -> UInt64? { numberOrNil(key)?.uint64Value }
+	public func numberOrNil(_ key: String) -> NSNumber? { value(key) }
+	public func dictionaryOrNil(_ key: String) -> [String: Any]? { value(key) }
 	
-	public func floatOrNil(_ key: String) -> Float? {
-		guard let value = self[key] else { return nil }
-		
-		// A float might be parsed as an NSNumber by the JSON parser.
-		if let value = value as? NSNumber {
-			return value.floatValue
-		}
-		
-		return (value as! Float)
-		
-	}
-	
-	public func uintOrNil(_ key: String) -> UInt? { return value(key) }
-	public func uint32OrNil(_ key: String) -> UInt32? { return value(key) }
-	
-	public func uint64OrNil(_ key: String) -> UInt64? {
-		guard let value = self[key] else { return nil }
-		
-		// A uint64 may be parsed as an NSNumber by the JSON parser.
-		if let value = value as? NSNumber {
-			return value.uint64Value
-		}
-		
-		return (value as! UInt64)
-	}
-	
-	public func numberOrNil(_ key: String) -> NSNumber? { return value(key) }
-	public func dictionaryOrNil(_ key: String) -> [String: Any]? { return value(key) }
-	
-	
-	
-	public func int(_ key: String) -> Int { return value(key)! }
-	public func bool(_ key: String) -> Bool { return value(key)! }
-	public func string(_ key: String) -> String { return value(key)! }
-	public func float(_ key: String) -> Float { return floatOrNil(key)! }
-	public func uint(_ key: String) -> UInt { return value(key)! }
-	public func uint32(_ key: String) -> UInt32 { return value(key)! }
-	public func uint64(_ key: String) -> UInt64 { return uint64OrNil(key)! }
-	public func number(_ key: String) -> NSNumber { return value(key)! }
-	public func dictionary(_ key: String) -> [String: Any] { return value(key)! }
+	public func int(_ key: String) -> Int { intOrNil(key)! }
+	public func bool(_ key: String) -> Bool { boolOrNil(key)! }
+	public func string(_ key: String) -> String { value(key)! }
+	public func float(_ key: String) -> Float { floatOrNil(key)! }
+	public func uint(_ key: String) -> UInt { uintOrNil(key)! }
+	public func uint32(_ key: String) -> UInt32 { uint32OrNil(key)! }
+	public func uint64(_ key: String) -> UInt64 { uint64OrNil(key)! }
+	public func number(_ key: String) -> NSNumber { value(key)! }
+	public func dictionary(_ key: String) -> [String: Any] { value(key)! }
 }
