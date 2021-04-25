f1bfca2780c492a37499cd2a0128ec10038ad99e
Todor Pitekov <todor.pitekov@pcloud.com>
2021-04-25 11:54:00 +0300

    remove comments

--- Package.swift
+++ Package.swift
@@ -11,12 +11,9 @@ let package = Package(
 		.iOS(.v9)
 	],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "PCloudSDKSwift", targets: ["PCloudSDKSwift"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.target(name: "PCloudSDKSwift", dependencies: [], path: "PCloudSDKSwift/Source", exclude: ["macOS"]),
 		.testTarget(name: "PCloudSDKSwiftTests", dependencies: ["PCloudSDKSwift"], path: "PCloudSDKSwift/Tests"),
     ]
